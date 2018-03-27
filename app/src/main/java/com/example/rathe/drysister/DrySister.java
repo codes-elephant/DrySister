4b059c96a0bc47d80ddfb478786501afafcf1649
coder-elephant <ratherout@163.con>
2018-03-27 17:20:17 +0800

    DrySisterFirst

---
+++ app/src/main/java/com/example/rathe/drysister/DrySister.java
@@ -0,0 +1,13 @@
+package com.example.rathe.drysister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrySister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dry_sister);
+    }
+}
